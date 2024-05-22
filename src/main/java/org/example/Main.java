b89f7c2665e8a126510cd752bdeca600fafbeee2
pespinas <pespinas99@gmail.com>
2024-05-22 13:49:18 +0200

    ApiCall and ApiFilterPokemon

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,38 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+import pokeApi.PokeApiClient;
+import pokeApi.Pokemon;
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        PokeApiClient.getPokemonInfo("sceptile", new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String responseData = response.body().string();
+                    JsonObject jsonObject = new Gson().fromJson(responseData, JsonObject.class);
+                    Pokemon pokemon = Pokemon.fromJson(jsonObject);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+                    // Accede a los atributos del Pokémon y muestra la información
+                    System.out.println("Name: " + pokemon.getName());
+                    System.out.println("Types: " + pokemon.getTypes());
+                    System.out.println("Generation: " + pokemon.getGen());
+                } else {
+                    System.out.println("Error: " + response.code());
+                }
+            }
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
